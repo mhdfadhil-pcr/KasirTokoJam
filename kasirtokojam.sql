8806faa9d57768b14524091cc5635f5cdf349bb3
Muhammad Fadhil <85356263+mhdfadhil-pcr@users.noreply.github.com>
2022-07-22 09:08:44 +0700

    Add files via upload

---
+++ kasirtokojam.sql
@@ -0,0 +1,339 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Waktu pembuatan: 21 Jul 2022 pada 15.44
+-- Versi server: 5.7.24
+-- Versi PHP: 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `joki_kasir`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `akses`
+--
+
+CREATE TABLE `akses` (
+  `id_akses` int(11) NOT NULL,
+  `nama_akses` varchar(25) NOT NULL,
+  `deskripsi` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `akses`
+--
+
+INSERT INTO `akses` (`id_akses`, `nama_akses`, `deskripsi`) VALUES
+(1, 'Admin', 'sebagai  pengelola kendali penuh pada sistem aplikasi'),
+(2, 'Staff', 'sebagai pengelola sistem stok barang, penjualan');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `bank`
+--
+
+CREATE TABLE `bank` (
+  `id` int(11) NOT NULL,
+  `nama_bank` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `bank`
+--
+
+INSERT INTO `bank` (`id`, `nama_bank`) VALUES
+(1, 'MANDIRI'),
+(2, 'BNI'),
+(3, 'BCA'),
+(4, 'BRI'),
+(5, 'CIMB Niaga');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` int(11) NOT NULL,
+  `nama_barang` varchar(30) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `ukuran` varchar(5) NOT NULL,
+  `harga` int(20) NOT NULL,
+  `foto` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_penjualan`
+--
+
+CREATE TABLE `detail_penjualan` (
+  `id` int(11) NOT NULL,
+  `no_trf` varchar(50) NOT NULL,
+  `nama_pelanggan` varchar(35) NOT NULL,
+  `totalpure` bigint(20) NOT NULL,
+  `grand_total` bigint(20) NOT NULL,
+  `diskon` int(3) NOT NULL,
+  `bayar` bigint(20) NOT NULL,
+  `kembalian` bigint(20) NOT NULL,
+  `catatan` varchar(50) NOT NULL,
+  `tgl_trf` date NOT NULL,
+  `jam_trf` time NOT NULL,
+  `id_pembayaran` int(2) NOT NULL,
+  `no_rek` int(18) DEFAULT NULL,
+  `atas_nama` varchar(35) NOT NULL,
+  `id_bank` int(2) DEFAULT NULL,
+  `operator` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
+(21, 'Jam Wanita'),
+(22, 'Jam Pria'),
+(23, 'Jam Analog'),
+(24, 'Jam Digital');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `operator`
+--
+
+CREATE TABLE `operator` (
+  `id_operator` int(11) NOT NULL,
+  `nama_operator` varchar(50) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `id_akses` int(3) NOT NULL,
+  `last_login` date NOT NULL,
+  `foto` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `operator`
+--
+
+INSERT INTO `operator` (`id_operator`, `nama_operator`, `username`, `password`, `id_akses`, `last_login`, `foto`) VALUES
+(9, 'M Fadil', 'fadil', '21232f297a57a5a743894a0e4a801fc3', 1, '2022-07-21', '03f5827b5b4d9f8468efd999a6022eee.png');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id_byr` int(2) NOT NULL,
+  `metode` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`id_byr`, `metode`) VALUES
+(1, 'Cash'),
+(2, 'Transfer');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_dtlpen` int(5) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `jumlah_stok` int(11) NOT NULL,
+  `harga_barang` bigint(20) NOT NULL,
+  `sub_total` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `stok`
+--
+
+CREATE TABLE `stok` (
+  `id_stok` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `stok_barang` int(11) NOT NULL,
+  `tanggal_stok` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `ukuran`
+--
+
+CREATE TABLE `ukuran` (
+  `id_ukuran` int(11) NOT NULL,
+  `nama_ukuran` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `ukuran`
+--
+
+INSERT INTO `ukuran` (`id_ukuran`, `nama_ukuran`) VALUES
+(1, '38mm'),
+(2, '40mm'),
+(3, '42mm'),
+(4, '44mm'),
+(5, '46mm'),
+(6, 'No Size');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `akses`
+--
+ALTER TABLE `akses`
+  ADD PRIMARY KEY (`id_akses`);
+
+--
+-- Indeks untuk tabel `bank`
+--
+ALTER TABLE `bank`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `detail_penjualan`
+--
+ALTER TABLE `detail_penjualan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `operator`
+--
+ALTER TABLE `operator`
+  ADD PRIMARY KEY (`id_operator`);
+
+--
+-- Indeks untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id_byr`);
+
+--
+-- Indeks untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- Indeks untuk tabel `stok`
+--
+ALTER TABLE `stok`
+  ADD PRIMARY KEY (`id_stok`);
+
+--
+-- Indeks untuk tabel `ukuran`
+--
+ALTER TABLE `ukuran`
+  ADD PRIMARY KEY (`id_ukuran`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `akses`
+--
+ALTER TABLE `akses`
+  MODIFY `id_akses` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `bank`
+--
+ALTER TABLE `bank`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `detail_penjualan`
+--
+ALTER TABLE `detail_penjualan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT untuk tabel `operator`
+--
+ALTER TABLE `operator`
+  MODIFY `id_operator` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id_byr` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
+--
+-- AUTO_INCREMENT untuk tabel `stok`
+--
+ALTER TABLE `stok`
+  MODIFY `id_stok` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+
+--
+-- AUTO_INCREMENT untuk tabel `ukuran`
+--
+ALTER TABLE `ukuran`
+  MODIFY `id_ukuran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
